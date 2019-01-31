c6d5ea607b57f09c70a1c91bde50b77483dc1e1f
duyng404 <4320964+duyng404@users.noreply.github.com>
2019-01-31 00:02:42 -0600

    admin dash can now generate redeemable codes & view all of them

--- src/containers/Queue.js
+++ src/containers/Queue.js
@@ -80,7 +80,6 @@ class Queue extends React.Component {
 		// fetch the current queue
 		api.fetchUrl("/api/admin/view-queue")
 		.then(response => {
-			this.extractData(response.data);
 			if (!response.data || !Array.isArray(response.data) || response.data.length === 0) {
 				this.setState({q:[]});
 				return;
--- src/containers/Redeemables.js
+++ src/containers/Redeemables.js
@@ -1,13 +1,58 @@
 import React from 'react';
+import Moment from 'moment';
 import * as api from '../utils/api';
 
 class Generate extends React.Component {
 	constructor(props) {
 		super(props);
+		this.generateFiveCodes = this.generateFiveCodes.bind(this);
 		this.state = {
-			allCodes: [],
-		}
+			availableCodes: [],
+			redeemedCodes: [],
+			newCodes: [],
+		};
 	}
+
+	generateFiveCodes(e) {
+		e.preventDefault();
+		api.postUrl("/api/admin/generate-five-codes")
+		.then(response => {
+			if (!response.data || !Array.isArray(response.data) || response.data.length === 0) {
+				this.setState({
+					newCodes: [],
+				});
+				return;
+			}
+			this.setState(prevState => ({
+				newCodes: response.data,
+				availableCodes: [...prevState.availableCodes, ...response.data],
+			}))
+		}).catch(err => {
+			console.log(err);
+		});
+	}
+
+	componentDidMount() {
+		// fetch all the codes
+		api.fetchUrl("/api/admin/view-redeemable-codes")
+		.then(response => {
+			if (!response.data || !Array.isArray(response.data) || response.data.length === 0) {
+				this.setState({
+					availableCodes: [],
+					redeemedCodes: [],
+				});
+				return;
+			}
+			this.setState({
+				availableCodes: response.data.filter(v => v.status === 1 || v.status === 2),
+				redeemedCodes: response.data.filter(v => v.status === 3),
+			})
+		})
+		.catch(err => {
+			console.log(err);
+		})
+	}
+
 	render () {
 		return (
 			<>
@@ -15,15 +60,13 @@ class Generate extends React.Component {
 					<h2 className="font-weight-bold">Generate Redeemable Codes</h2>
 					<div className="my-4"></div>
 					<p className="lead">Click the button below to generate five redeemable codes. Remember to write them down afterwards.</p>
-					<button className="btn btn-primary btn-lg" type="button">Generate 5 Redeemable Codes</button>
+					<button className="btn btn-primary btn-lg" type="button" onClick={this.generateFiveCodes}>Generate 5 Redeemable Codes</button>
 				</section>
 
 				<section className="container mt-5 text-center">
-					<h3 className="my-5">CA-ABCD-1234-ABCD</h3>
-					<h3 className="my-5">CA-ABCD-1234-ABCD</h3>
-					<h3 className="my-5">CA-ABCD-1234-ABCD</h3>
-					<h3 className="my-5">CA-ABCD-1234-ABCD</h3>
-					<h3 className="my-5">CA-ABCD-1234-ABCD</h3>
+					{ this.state.newCodes.map(code => (
+						<h3 className="my-5 ca-mono-font">{code.code}</h3>
+					))}
 				</section>
 
 				<hr className="my-5" />
@@ -35,15 +78,19 @@ class Generate extends React.Component {
 							<tr>
 								<th scope="col">#</th>
 								<th scope="col">Generated</th>
+								<th scope="col">Amount</th>
 								<th scope="col">Status</th>
 							</tr>
 						</thead>
 						<tbody>
-							<tr>
-								<th scope="row"></th>
-								<td></td>
-								<td></td>
-							</tr>
+							{ this.state.availableCodes.map(code => (
+								<tr>
+									<th scope="row">{code.code}</th>
+									<td>{Moment(code.created_at).format("MMM Y h:mmA")}</td>
+									<td>$10</td>
+									<td className="text-success">Available</td>
+								</tr>
+							))}
 						</tbody>
 					</table>
 				</section>
@@ -52,7 +99,30 @@ class Generate extends React.Component {
 
 				<section className="container mt-5 text-center">
 					<h2 className="font-weight-bold">Redeemed Codes</h2>
-
+					<table class="table table-responsive-md">
+						<thead>
+							<tr>
+								<th scope="col">#</th>
+								<th scope="col">Generated</th>
+								<th scope="col">Redeemed</th>
+								<th scope="col">By</th>
+								<th scope="col">Amount</th>
+								<th scope="col">Status</th>
+							</tr>
+						</thead>
+						<tbody>
+							{ this.state.redeemedCodes.map(code => (
+								<tr>
+									<th scope="row">{code.code}</th>
+									<td>{Moment(code.created_at).format("MMM Y h:mmA")}</td>
+									<td>{Moment(code.redeemed_at).format("MMM Y h:mmA")}</td>
+									<td>{code.redeemed_by_user.gus_username}</td>
+									<td>$10</td>
+									<td className="text-danger">Redeemed</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
 				</section>
 			</>
 		)
