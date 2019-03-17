f6067ccd62b9c9edcad301a758bbe912e0f3cca7
duyng404 <4320964+duyng404@users.noreply.github.com>
2019-03-17 13:45:16 -0500

    orders page select date feature

--- src/containers/Orders.js
+++ src/containers/Orders.js
@@ -2,13 +2,18 @@ import React from 'react';
 import * as api from '../utils/api';
 import queryString from 'query-string';
 import OrdersTable from '../components/OrdersTable';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import moment from 'moment';
 
 class Orders extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			orders: [],
+			currentDate: null,
 		}
+		this.handleDatePickerChange = this.handleDatePickerChange.bind(this);
 		// this.handleViewDetailsUser = this.handleViewDetailsUser.bind(this);
 		// this.handleFormChange = this.handleFormChange.bind(this);
 		// this.handleFormSubmit = this.handleFormSubmit.bind(this);
@@ -16,24 +21,24 @@ class Orders extends React.Component {
 
 		// get all query params
 		const value = queryString.parse(this.props.location.search);
-		if (value.fullname && value.fullname !== "") this.state.fullname = value.fullname;
-		if (value.gususername && value.gususername !== "") this.state.gususername = value.gususername;
-		if (value.sortBy && value.sortBy !== "") this.state.sortBy = value.sortBy;
+		if (value.date && value.date !== "") {
+			// check legit date in iso8601 format
+			const dateraw = moment(value.date);
+			if (dateraw.toString() !== "Invalid date") {
+				this.state.dateRaw = dateraw.toDate();
+			}
+		}
 	}
 
-	// // this helper function will build the query params part of the url and return it, using the state
-	// buildQueryParams() {
-	// 	const params = {
-	// 		fullname: this.state.fullname,
-	// 		gususername: this.state.gususername,
-	// 		sortBy: this.state.sortBy,
-	// 	};
-	// 	// if param is their default value, remove it by setting it to undefined
-	// 	if (params.fullname === "") params.fullname = undefined;
-	// 	if (params.gususername === "") params.gususername = undefined;
-	// 	if (params.sortBy === "id") params.sortBy = undefined;
-	// 	return queryString.stringify(params);
-	// }
+	// this helper function will build the query params part of the url and return it, using the state
+	buildQueryParams() {
+		const params = {
+			date: moment(this.state.currentDate).toISOString(true),
+		};
+		// if param is their default value, remove it by setting it to undefined
+		if (this.state.currentDate === null) params.date = undefined;
+		return queryString.stringify(params);
+	}
 
 	// handleFormChange(e) {
 	// 	const name = e.target.name;
@@ -70,9 +75,12 @@ class Orders extends React.Component {
 	// 		});
 	// }
 
-	componentDidMount() {
-		// do a get request
-		api.fetchUrl(`${process.env.REACT_APP_BACKEND_URL}/api/admin/orders-last-12-hours`)
+	fetchData() {
+		const params = this.buildQueryParams();
+		const url = `/api/admin/view-orders?${params}`;
+		console.log(url);
+		this.props.history.push(`/orders?${params}`);
+		api.fetchUrl(url)
 			.then(res => {
 				console.log(res.data);
 				this.setState({ orders: res.data });
@@ -83,61 +91,48 @@ class Orders extends React.Component {
 			});
 	}
 
+	handleDatePickerChange(date) {
+		this.setState({ currentDate: date }, this.fetchData);
+		// console.log("changing date to:", date);
+		// console.log(moment(date).utc().toISOString());
+	}
+
+	componentDidMount() {
+		// do a get request
+		this.fetchData()
+	}
+
 	render() {
 		return (
 			<div className="ca-container">
 				<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
 					<h1 className="h2 font-weight-bold">Orders Management</h1>
-					{/* <div>abcxyz</div> */}
-				</div>
-				{/* <div className="card">
-					<div className="card-body">
-						<h3>Search filters</h3>
-						<form onSubmit={this.handleFormSubmit}>
-							<div className="form-row">
-								<div className="col-4">
-									<label htmlFor="filter-name">Full Name</label>
-									<input onChange={this.handleFormChange} type="text" className="form-control" placeholder="eg. Jon Smith" name="fullname" id="fullname" value={this.state.fullname} />
-								</div>
-								<div className="col-4">
-									<label htmlFor="filter-name">Gus Username</label>
-									<input onChange={this.handleFormChange} type="text" className="form-control" placeholder="eg. jsmith3" name="gususername" id="gususername" value={this.state.gususername} />
-								</div>
-								<div className="col-4">
-									<label htmlFor="filter-sort">Sort By</label>
-									<select onChange={this.handleFormChange} name="sortBy" value={this.state.sortBy} className="form-control" id="filter-sort">
-										<option value="id">ID ascending</option>
-										<option value="idDESC">ID descending</option>
-										<option value="full_name">Full Name A-Z</option>
-										<option value="full_nameDESC">Full Name Z-A</option>
-										<option value="gus_username">Gus Username A-Z</option>
-										<option value="gus_usernameDESC">Gus Username Z-A</option>
-									</select>
-								</div>
-							</div>
-							<div className="d-flex justify-content-end mt-2">
-								<button className="btn btn-primary" type="submit">Search</button>
-							</div>
-						</form>
+					<div>
+						<span>Select a date: </span>
+						<DatePicker
+							className="d-inline-block form-control"
+							selected={this.state.currentDate}
+							onChange={this.handleDatePickerChange}
+							todayButton={"Today"}
+							isClearable={true}
+							placeholderText="not selected"
+							popperModifiers={{
+								preventOverflow: {
+								  enabled: true,
+								  escapeWithReference: false, // force popper to stay in viewport (even when input is scrolled out of view)
+								  boundariesElement: 'viewport'
+								}
+							  }}/>
 					</div>
 				</div>
 
-				<div className="my-4"></div> */}
-
-				<h3>All orders last 12 hours</h3>
+				<h4 className="my-4 text-center">Currently showing: {this.state.currentDate === null ? 'All orders last 24 hours' : `All orders on ${moment(this.state.currentDate).format("MMM D h:mmA")}`}</h4>
 				{this.state.orders.length > 0 ?
 					<OrdersTable data={this.state.orders} />
 					:
 					<p className="text-muted">No results</p>
 				}
-				{/* <div className="my-4"></div>
-				<h3 id="user-details">User Details</h3>
-				{Object.keys(this.state.userInfo).length !== 0 ?
-					<UserInfo user={this.state.userInfo} />
-					:
-					<p className="text-muted">No user selected</p>
-				} */}
-				<p className="text-muted">More features will come ...</p>
+
 			</div>
 		);
 	}
